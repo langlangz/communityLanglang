84c8775b33a588b697c1b32b9c054f2a79af70b2
langlangz <470487197@qq.com>
2021-08-02 15:25:03 +0800

    init commit

---
+++ src/main/java/life/langlang/community/communitylanglang/CommunityLanglangApplication.java
@@ -0,0 +1,13 @@
+package life.langlang.community.communitylanglang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunityLanglangApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommunityLanglangApplication.class, args);
+    }
+
+}
---
+++ src/test/java/life/langlang/community/communitylanglang/CommunityLanglangApplicationTests.java
@@ -0,0 +1,13 @@
+package life.langlang.community.communitylanglang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityLanglangApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
